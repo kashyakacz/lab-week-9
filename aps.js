0dd738d9c779588759c92b06f618438c3b3f5cd6
Kashya Kaczkowska <kashya.kacz55@yahoo.ca>
2021-03-12 20:58:22 -0800

    conditional scripting + declared variables

---
+++ aps.js
@@ -0,0 +1,17 @@
+num1 = 1
+num2 = 2
+
+const animal = "octupus"
+const num4 = 4.24
+
+if(num1 == num2 && num1){
+    console.log(typeof(num1) == typeof(num2));
+    console.log("they match")
+}
+if(num1 != num2){
+    console.log("no luck")
+}
+
+for (i = 0; i <= animal.length; i++){
+    console.log(i)
+}
